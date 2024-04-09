635d20e3713de1cf198dafcf445b24823ac7c1f3
omkar2254 <omkarpatil2254@gmail.com>
2024-04-09 21:52:26 +0530

    comment added

---
+++ src/Components/comments.js
@@ -0,0 +1,26 @@
+{/* <div class="m-4 grid sm:grid-cols-2 gap-4">
+  <div class="min-h-[100px] rounded-lg shadow bg-orange-400">Hey, if you dividing two col make sure that each div has same space</div>
+  <div class="min-h-[100px] rounded-lg shadow bg-purple-400">Hey hii</div>
+</div>
+
+
+<div class="m-4 grid grid-cols-2 sm:grid-cols-2 gap-4">
+  <div class="min-h-[100px] rounded-lg shadow bg-lime-400">Here I explicitly give 2 grid cols</div>
+  <div class="min-h-[100px] rounded-lg shadow bg-red-400"></div>
+</div>
+
+<div class="m-4 grid gap-4 grid-cols-1 sm:grid-cols-2">
+  <div class="min-h-[100px] rounded-lg shadow bg-black"></div>
+  <div class="min-h-[100px] rounded-lg shadow bg-slate-400"></div>
+</div>
+
+<div class="m-4 grid grid-cols-1 sm:grid-cols-12 gap-4">
+  <div class="min-h-[100px] rounded-lg shadow bg-blue-500 sm:col-span-4"></div>
+  <div class="min-h-[100px] rounded-lg shadow bg-blue-500 sm:col-span-8"></div>
+</div>
+
+<div class="m-4 grid grid-cols-1 sm:grid-cols-12 gap-4">
+  <div class="min-h-[100px] rounded-lg shadow bg-green-800 sm:col-span-2"></div>
+  <div class="min-h-[100px] rounded-lg shadow bg-green-800 sm:col-span-8"></div>
+  <div class="min-h-[100px] rounded-lg shadow bg-green-800 sm:col-span-2"></div>
+</div> */}
